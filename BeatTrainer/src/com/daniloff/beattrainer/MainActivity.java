c8c797a26842123793feac8025d288336b685ef0
Arc.B.Daniloff <arc.b.daniloff@gmail.com>
2013-08-18 14:56:02 +0700

    trim

--- BeatTrainer/src/com/daniloff/beattrainer/MainActivity.java
+++ BeatTrainer/src/com/daniloff/beattrainer/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		logic = new SoundLooperImpl(getApplicationContext());
-
 		start();
 	}
 
@@ -49,93 +48,20 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.start_button:
-//			logic.loopSound();
-	        AudioGenerator audio = new AudioGenerator(8000);
+			// logic.loopSound();
+			AudioGenerator audio = new AudioGenerator(8000);
 
-	        
+			int noteDuration = 2400;
+			double[] silence = audio.getSineWave(noteDuration, 8000, 0);// was
+																		// 200
+			double[] laNote = audio.getSineWave(noteDuration, 8000, 880.00);
 
-	        int noteDuration = 2400;
-	        
-	        double[] silence = audio.getSineWave(noteDuration, 8000, 0);//was 200
-
-	        double[] doNote = audio.getSineWave(noteDuration/2, 8000, 523.25);
-	        double[] reNote = audio.getSineWave(noteDuration/2, 8000, 587.33);
-	        double[] faNote = audio.getSineWave(noteDuration, 8000, 698.46);
-	        double[] laNote = audio.getSineWave(noteDuration, 8000, 880.00);
-	        double[] laNote2 =
-	                audio.getSineWave((int) (noteDuration*1.25), 8000, 880.00);
-	        double[] siNote = audio.getSineWave(noteDuration/2, 8000, 987.77);
-	        double[] doNote2 =
-	                audio.getSineWave((int) (noteDuration*1.25), 8000, 523.25);
-	        double[] miNote = audio.getSineWave(noteDuration/2, 8000, 659.26);
-	        double[] miNote2 = audio.getSineWave(noteDuration, 8000, 659.26);
-	        double[] doNote3 = audio.getSineWave(noteDuration, 8000, 523.25);
-	        double[] miNote3 = audio.getSineWave(noteDuration*3, 8000, 659.26);
-	        double[] reNote2 = audio.getSineWave(noteDuration*4, 8000, 587.33);
-
-	        audio.createPlayer();
-	        for(int i=0;i<90;i++){
-	        audio.writeSound(doNote);
-	        audio.writeSound(silence);}
-	    /*    audio.writeSound(reNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(faNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(laNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(laNote2);
-	        audio.writeSound(silence);
-	        audio.writeSound(siNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(laNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(faNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(doNote2);
-	        audio.writeSound(silence);
-	        audio.writeSound(miNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(faNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(faNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(miNote2);
-	        audio.writeSound(silence);
-	        audio.writeSound(doNote3);
-	        audio.writeSound(silence);
-	        audio.writeSound(miNote3);
-	        audio.writeSound(silence);
-	        audio.writeSound(doNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(reNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(faNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(laNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(laNote2);
-	        audio.writeSound(silence);
-	        audio.writeSound(siNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(laNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(faNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(doNote2);
-	        audio.writeSound(silence);
-	        audio.writeSound(miNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(faNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(faNote);
-	        audio.writeSound(silence);
-	        audio.writeSound(miNote2);
-	        audio.writeSound(silence);
-	        audio.writeSound(miNote2);
-	        audio.writeSound(silence);
-	        audio.writeSound(reNote2);*/
-
-	        audio.destroyAudioTrack();
+			audio.createPlayer();
+			for (int i = 0; i < 90; i++) {
+				audio.writeSound(laNote);
+				audio.writeSound(silence);
+			}
+			audio.destroyAudioTrack();
 
 			layout.setBackgroundColor(Color.MAGENTA);
 			break;
@@ -143,18 +69,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		case R.id.drum_button:
 			drumButton.setBackgroundColor(Color.RED);
 
-			try {
-				flash();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
 		}
-
 	}
-
-	private void flash() throws InterruptedException {
-		startButton.setBackgroundColor(Color.RED);
-		layout.setBackgroundColor(Color.GREEN);
-	}
-
 }
