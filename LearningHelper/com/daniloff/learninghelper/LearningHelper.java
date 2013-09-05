a282f2cffccdbfa01505d888bb3350a548d4b61c
Arc.B.Daniloff <arc.b.daniloff@gmail.com>
2013-09-05 22:54:06 +0700

    learn helper preparation

--- LearningHelper/com/daniloff/learninghelper/LearningHelper.java
+++ LearningHelper/com/daniloff/learninghelper/LearningHelper.java
@@ -6,18 +6,35 @@ import java.util.List;
 public class LearningHelper {
 
 	public static void main(String[] args) {
-		prepareTaskList();
+		List<Task>taskList=prepareTaskList();
+		for(int i=0;i<2;i++){
+			runLoop(taskList);
+		}
 
 	}
 
-	private static void prepareTaskList() {
-		String FileAsString = FileUtils.readFile("config/words.txt");
-		String[] sharpSeparatedWords = FileAsString.split("//n");
-		List<String> list = new ArrayList<>();
-		for (String wordsCouples : sharpSeparatedWords) {
-			list.add(wordsCouples);
+	private static void runLoop(List<Task> taskList) {
+		while  (!taskList.isEmpty()){
+		Task currentTask=taskList.remove(0);
+			System.out.println(currentTask.getMeaning());
+			System.out.println(currentTask.getPhrase());
+			System.out.println();
+			
 		}
+		
+	}
 
+	private static List<Task> prepareTaskList() {
+		String FileAsString = FileUtils.readFile("config/words.txt");
+		String[] sharpSeparatedWords = FileAsString.split("\\n");
+		
+		List<Task> taskList = new ArrayList<>();
+		
+		for (String wordsCouple : sharpSeparatedWords) {
+			Task task=new Task(wordsCouple);
+			taskList.add(task);
+		}
+		return taskList;
 	}
 
 }
--- LearningHelper/com/daniloff/learninghelper/Task.java
+++ LearningHelper/com/daniloff/learninghelper/Task.java
@@ -2,4 +2,21 @@ package com.daniloff.learninghelper;
 
 public class Task {
 
+	private final String phrase;
+	private final String meaning;
+
+	public Task(String wordsCouples) {
+		int separatorIndex = wordsCouples.indexOf('#');
+		phrase = wordsCouples.substring(0, separatorIndex);
+		meaning = wordsCouples.substring(separatorIndex + 1);
+	}
+
+	public String getPhrase() {
+		return phrase;
+	}
+
+	public String getMeaning() {
+		return meaning;
+	}
+
 }
