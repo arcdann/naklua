d7767c5321a38123a17642cf4d3979f4f423d12f
Arc.B.Daniloff <arc.b.daniloff@gmail.com>
2013-09-05 23:26:47 +0700

    learn helper finished

--- LearningHelper/com/daniloff/learninghelper/LearningHelper.java
+++ LearningHelper/com/daniloff/learninghelper/LearningHelper.java
@@ -1,37 +1,56 @@
 package com.daniloff.learninghelper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LearningHelper {
 
 	public static void main(String[] args) {
-		List<Task>taskList=prepareTaskList();
-		for(int i=0;i<2;i++){
+		List<Task> taskList = prepareTaskList();
+		for (int i = 0; i < 4; i++) {
 			runLoop(taskList);
+			System.out.println();
 		}
-
+		System.out.println("End of exercise");
 	}
 
 	private static void runLoop(List<Task> taskList) {
-		while  (!taskList.isEmpty()){
-		Task currentTask=taskList.remove(0);
-			System.out.println(currentTask.getMeaning());
-			System.out.println(currentTask.getPhrase());
+		List<Task> currentTaskList = new ArrayList<>(taskList);
+		Collections.shuffle(currentTaskList);
+		while (!currentTaskList.isEmpty()) {
+			Task currentTask = currentTaskList.remove(0);
+			System.out.print(currentTask.getMeaning() + " : ");
+
+			String answer = "";
+			BufferedReader bfReader = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				answer = bfReader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			if (answer.equalsIgnoreCase(currentTask.getPhrase())) {
+				System.out.println("Correct");
+			} else {
+				System.out.println("Wrong. The phrase is to be: " + currentTask.getPhrase());
+				currentTaskList.add(currentTask);
+			}
 			System.out.println();
-			
 		}
-		
 	}
 
 	private static List<Task> prepareTaskList() {
 		String FileAsString = FileUtils.readFile("config/words.txt");
 		String[] sharpSeparatedWords = FileAsString.split("\\n");
-		
+
 		List<Task> taskList = new ArrayList<>();
-		
+
 		for (String wordsCouple : sharpSeparatedWords) {
-			Task task=new Task(wordsCouple);
+			Task task = new Task(wordsCouple);
 			taskList.add(task);
 		}
 		return taskList;
