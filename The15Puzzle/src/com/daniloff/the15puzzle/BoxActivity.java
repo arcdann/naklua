020d414bc4ee532b2b90d88df78859ceeaadb7c1
Arc.B.Daniloff <arc.b.daniloff@gmail.com>
2013-11-05 02:15:55 +0800

    15 pause added

--- The15Puzzle/src/com/daniloff/the15puzzle/BoxActivity.java
+++ The15Puzzle/src/com/daniloff/the15puzzle/BoxActivity.java
@@ -1,10 +1,9 @@
 package com.daniloff.the15puzzle;
 
-import android.R.color;
-import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -35,8 +34,10 @@ public class BoxActivity extends Activity implements OnClickListener {
 	private Button[][] chips;
 	private int[][] cells;// ///////////////////////////////////
 
-	private int xEmpty = X - 1;
-	private int yEmpty = Y - 1;
+	private int emptyX;
+	private int emptyY;
+
+	private boolean paused;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +48,7 @@ public class BoxActivity extends Activity implements OnClickListener {
 		newGameButton = (Button) findViewById(R.id.buttonNewGame);
 		newGameButton.setOnClickListener(this);
 		pauseButton = (Button) findViewById(R.id.buttonPause);
+		pauseButton.setOnClickListener(this);
 		moveCountView = (TextView) findViewById(R.id.movesCountView);
 
 		infoView = (TextView) findViewById(R.id.textView1);
@@ -55,34 +57,35 @@ public class BoxActivity extends Activity implements OnClickListener {
 
 			@Override
 			public void onClick(View v) {
+				if (paused)
+					return;
 				moveCount++;
 				moveCountView.setText("" + moveCount);
 				int id = v.getId();
 
-				int xClicked = id % ((id / 10) * 10);
-				int yClicked = id % ((id / 100) * 100) / 10;
+				int comingX = id % ((id / 10) * 10);
+				int comingY = id % ((id / 100) * 100) / 10;
 
-				if ((xClicked == xEmpty&&Math.abs(yClicked-yEmpty)==1) ||( yClicked == yEmpty &&Math.abs(xClicked-xEmpty)==1)){
-					Button bDon=(Button) findViewById(CHIP_ID_PREXIX * 100 + yEmpty * 10 + xEmpty);
-					Button bAcc=(Button) findViewById(CHIP_ID_PREXIX * 100 + yClicked * 10 + xClicked);
-					String buf=bDon.getText().toString();
-					bDon.setText((CharSequence) ((TextView) findViewById(CHIP_ID_PREXIX * 100 + yClicked * 10 + xClicked)).getText().toString());
-					bAcc.setText(buf);
-					bDon.setVisibility(View.VISIBLE);
-					xEmpty = xClicked;
-					yEmpty = yClicked;
-					infoView.setText(v.getId() + " x=" + xClicked + " y=" + yClicked);
-					((Button) v).setVisibility(View.INVISIBLE);
+				if ((comingX == emptyX && Math.abs(comingY - emptyY) == 1)
+						|| (comingY == emptyY && Math.abs(comingX - emptyX) == 1)) {
+					// if (comingX == emptyX || comingY == emptyY) {
+
+					swapButtons(v, comingX, comingY);
 				}
-//				if(xClicked==xEmpty){
-//					int numX=xEmpty-xClicked;
-//					if(numX>0)
-//					for (int i=xEmpty;i<xClicked;i--){
-//						
-//					}
-//					
-//				}
 
+			}
+
+			private void swapButtons(View v, int comingX, int comingY) {
+				Button bEmpty = (Button) findViewById(CHIP_ID_PREXIX * 100 + emptyY * 10 + emptyX);
+				Button bComing = (Button) findViewById(CHIP_ID_PREXIX * 100 + comingY * 10 + comingX);
+				String buf = bEmpty.getText().toString();
+				bEmpty.setText(bComing.getText().toString());
+				bComing.setText(buf);
+
+				bEmpty.setVisibility(View.VISIBLE);
+				emptyX = comingX;
+				emptyY = comingY;
+				((Button) v).setVisibility(View.INVISIBLE);
 			}
 		};
 		createGameBox();
@@ -90,8 +93,12 @@ public class BoxActivity extends Activity implements OnClickListener {
 	}
 
 	private void createGameBox() {
+		emptyX = X - 1;
+		emptyY = Y - 1;
 		moveCount = 0;
+		paused = false;
 		moveCountView.setText("" + moveCount);
+		pauseButton.setText(R.string.buttonPause_text_Pause);
 
 		chips = new Button[X][Y];
 
@@ -137,7 +144,25 @@ public class BoxActivity extends Activity implements OnClickListener {
 			createGameBox();
 			break;
 		case R.id.buttonPause:
-			// /////////
+			if (!paused) {
+				paused = true;
+				pauseButton.setText(R.string.buttonPause_text_Resume);
+				for (int y = 0; y < Y; y++) {
+					for (int x = 0; x < X; x++) {
+						Button b = (Button) findViewById(CHIP_ID_PREXIX * 100 + y * 10 + x);
+						b.setTextColor(Color.GRAY);
+					}
+				}
+			} else {
+				paused = false;
+				pauseButton.setText(R.string.buttonPause_text_Pause);
+				for (int y = 0; y < Y; y++) {
+					for (int x = 0; x < X; x++) {
+						Button b = (Button) findViewById(CHIP_ID_PREXIX * 100 + y * 10 + x);
+						b.setTextColor(Color.BLACK);
+					}
+				}
+			}
 			break;
 
 		default:
