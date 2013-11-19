a637edb004e15d03e1e312f510c44752bdef9dd7
Arc.B.Daniloff <arc.b.daniloff@gmail.com>
2013-11-19 23:23:02 +0700

    stopWatch

--- StopWatch/src/com/daniloff/stopwatch/WatchActivity.java
+++ StopWatch/src/com/daniloff/stopwatch/WatchActivity.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 public class WatchActivity extends Activity implements OnClickListener, OnLongClickListener {
 
 	private long startTime = 0;
+	private long stopTime = 0;
 	private long restartTime = 0;
 	private long elapsedTime = 0;
 	private long elapsedDiffTime = 0;
@@ -33,8 +34,8 @@ public class WatchActivity extends Activity implements OnClickListener, OnLongCl
 	private Button resetButton;
 
 	private int point;
-	private CountDownTimer showDelayTimer;
-	private final long SHOW_DELAY = 1000;
+	private CountDownTimer delayTimerShow;
+	private final long SHOW_DELAY = 3000;
 
 	private Timer timer;
 
@@ -58,7 +59,7 @@ public class WatchActivity extends Activity implements OnClickListener, OnLongCl
 		resetButton.setEnabled(false);
 		resetButton.setOnLongClickListener(this);
 
-		showDelayTimer = new CountDownTimer(SHOW_DELAY, SHOW_DELAY) {
+		delayTimerShow = new CountDownTimer(SHOW_DELAY, SHOW_DELAY) {
 			@Override
 			public void onTick(long millisUntilFinished) {
 
@@ -66,8 +67,8 @@ public class WatchActivity extends Activity implements OnClickListener, OnLongCl
 
 			@Override
 			public void onFinish() {
-				// startClockViz();
 				timeView1.setEnabled(true);
+				timeView2.setEnabled(true);
 			}
 		};
 	}
@@ -95,7 +96,7 @@ public class WatchActivity extends Activity implements OnClickListener, OnLongCl
 		}, msec, 1000);
 
 		button1.setText("Stop");
-		button2.setText("Stop (" + point + ")");
+		button2.setText("Restart (" + point + ")");
 	}
 
 	private void startDiffClock() {
@@ -111,7 +112,7 @@ public class WatchActivity extends Activity implements OnClickListener, OnLongCl
 			}
 		};
 
-		int msec = 999 - (int) ((System.currentTimeMillis() - startTime) % 1000);
+		int msec = 999 - (int) ((System.currentTimeMillis() - restartTime) % 1000);
 		timer.scheduleAtFixedRate(new TimerTask() {
 			@Override
 			public void run() {
@@ -120,7 +121,7 @@ public class WatchActivity extends Activity implements OnClickListener, OnLongCl
 		}, msec, 1000);
 
 		button1.setText("Stop");
-		button2.setText("Stop (" + point + ")");
+		button2.setText("Restart (" + point + ")");
 	}
 
 	private String getElapsedTime() {
@@ -142,51 +143,71 @@ public class WatchActivity extends Activity implements OnClickListener, OnLongCl
 
 	@Override
 	public void onClick(View v) {
+		System.out.println("XXXXXXXXXXXXX");
 		switch (v.getId()) {
 		case R.id.button1:
 			if (!started) {
-				point++;
+				if(point==0){
+					point++;
+				}
 				startGeneralClock();
 				started = true;
 
 			} else {
-				long currentTime = System.currentTimeMillis();
-				elapsedTime += currentTime - startTime;
-				String timeFormatted = formatterMils.format(elapsedTime);
-				timeView1.setText(timeFormatted);
+				stopTime = System.currentTimeMillis();
+				elapsedTime += stopTime - startTime;
+				elapsedDiffTime += stopTime - restartTime;
+				String generalTimeFormatted = formatterMils.format(elapsedTime);
+				timeView1.setText(generalTimeFormatted);
+				String diffTimeFormatted = formatterMils.format(elapsedDiffTime);
+				timeView2.setText(diffTimeFormatted);
 				button1.setText("Resume");
 				started = false;
 				resetButton.setEnabled(true);
 			}
 			break;
 		case R.id.button2:
+			point++;
 			if (!started) {// start
 				startGeneralClock();
 				started = true;
 
-				point++;
+
+				
+	 
 
 			} else {
 				timeView2.setVisibility(View.VISIBLE);
+				elapsedDiffTime=0;
+				if(restartTime==0){
+					restartTime=startTime;
+				}
 				long currentTime = System.currentTimeMillis();
 				String timeFormattedCommon = formatterMils.format((currentTime - startTime));
 				String timeFormattedLocal = formatterMils.format((currentTime - restartTime));
 				restartTime = System.currentTimeMillis();
-				button2.setText("Stop (" + point + ")");
+				button2.setText("Restart (" + point + ")");
 				// //////////////////////////////////////////////////////////////////////
-				// timeView1.setText(timeFormattedCommon);
+				timeView1.setText(timeFormattedCommon);
 				// timeView1.setEnabled(false);
 				timeView2.setText(timeFormattedLocal);
 				// timeView2.setEnabled(false);
 				// showDelayTimer.start();
 				startDiffClock();
+				
+				
+				timeView1.setEnabled(false);
+				timeView2.setEnabled(false);
+				delayTimerShow.start();
+				printData();
+				
+				
 			}
 
 			break;
 		default:
 			break;
 		}
-printData();
 	}
 
 	private void printData() {
@@ -195,7 +216,7 @@ printData();
 		System.out.println(formatterMils.format(elapsedTime));
 		System.out.println(formatterMils.format(elapsedDiffTime));
 		System.out.println();
-		
+
 	}
 
 	public boolean isStarted() {
@@ -224,6 +245,10 @@ printData();
 		button1.setText("Start");
 		button2.setText("Start (" + point + ")");
 		elapsedTime = 0;
+		elapsedDiffTime = 0;
+		startTime=0;
+		restartTime=0;
+		stopTime=0;
 		resetButton.setEnabled(false);
 	}
 
